75a6bf6fcd4050df55f372ff46e7cf60732294ae
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-12-10 06:14:11 -0600

    fairly detached root

--- src/NebbishesOfSPLORR/Root.vb
+++ src/NebbishesOfSPLORR/Root.vb
@@ -1,39 +1,43 @@
 ﻿Public Class Root
     Inherits Game
-    Private uiScale As Integer = 4
-    Private graphics As GraphicsDeviceManager
-    Private spriteBatch As SpriteBatch
-    Private oldKeyboardState As New KeyboardState
-    Private keyboardState As New KeyboardState
-    Private Const ScreenWidth = 160
-    Private Const ScreenHeight = 90
-    Sub New()
-        graphics = New GraphicsDeviceManager(Me)
+    Private _uiScale As Integer
+    Private _graphics As GraphicsDeviceManager
+    Private _spriteBatch As SpriteBatch
+    Private _oldKeyboardState As New KeyboardState
+    Private _keyboardState As New KeyboardState
+    Private ReadOnly _viewWidth As Integer
+    Private ReadOnly _viewHeight As Integer
+    Private ReadOnly _windowTitle As String
+    Sub New(windowTitle As String, viewWidth As Integer, viewHeight As Integer, uiScale As Integer)
+        _uiScale = uiScale
+        _viewHeight = viewHeight
+        _viewWidth = viewWidth
+        _windowTitle = windowTitle
+        _graphics = New GraphicsDeviceManager(Me)
     End Sub
     Private Sub ResizeScreen()
-        graphics.PreferredBackBufferWidth = ScreenWidth * uiScale
-        graphics.PreferredBackBufferHeight = ScreenHeight * uiScale
-        graphics.ApplyChanges()
+        _graphics.PreferredBackBufferWidth = _viewWidth * _uiScale
+        _graphics.PreferredBackBufferHeight = _viewHeight * _uiScale
+        _graphics.ApplyChanges()
     End Sub
     Private Sub SetUIScale(newScale As Integer)
-        uiScale = newScale
+        _uiScale = newScale
         ResizeScreen()
     End Sub
     Protected Overrides Sub Initialize()
         MyBase.Initialize()
-        Window.Title = "Nebbishes of SPLORR!!"
-        Content.RootDirectory = "Content"
+        Window.Title = _windowTitle
         ResizeScreen()
     End Sub
     Protected Overrides Sub LoadContent()
         MyBase.LoadContent()
-        spriteBatch = New SpriteBatch(GraphicsDevice)
+        _spriteBatch = New SpriteBatch(GraphicsDevice)
     End Sub
     Protected Overrides Sub Update(gameTime As GameTime)
-        oldKeyboardState = keyboardState
-        keyboardState = Keyboard.GetState()
-        For Each key In keyboardState.GetPressedKeys()
-            If Not oldKeyboardState.IsKeyDown(key) Then
+        _oldKeyboardState = _keyboardState
+        _keyboardState = Keyboard.GetState()
+        For Each key In _keyboardState.GetPressedKeys()
+            If Not _oldKeyboardState.IsKeyDown(key) Then
                 'TODO: send to the input processor
             End If
         Next
@@ -43,9 +47,9 @@
     End Sub
     Protected Overrides Sub Draw(gameTime As GameTime)
         GraphicsDevice.Clear(Color.Black)
-        spriteBatch.Begin(samplerState:=SamplerState.PointClamp)
+        _spriteBatch.Begin(samplerState:=SamplerState.PointClamp)
         'TODO: draw things
-        spriteBatch.End()
+        _spriteBatch.End()
         MyBase.Draw(gameTime)
     End Sub
 End Class
