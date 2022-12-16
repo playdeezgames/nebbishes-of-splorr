53766fd90fd1b463c0451f240649f3bc01a02dd1
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-12-16 13:34:20 -0600

    thingie

---
+++ src/NOS.Business/Thingie/Thingie.vb
@@ -0,0 +1,11 @@
+﻿Public MustInherit Class Thingie
+    Implements IThingie
+    Protected ReadOnly _worldData As WorldData
+    Protected ReadOnly _world As World
+    Public ReadOnly Property Id As Integer Implements IThingie.Id
+    Friend Sub New(worldData As WorldData, world As World, id As Integer)
+        _worldData = worldData
+        _world = world
+        Me.Id = id
+    End Sub
+End Class
